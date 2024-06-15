5ab1e578cce0e404c7b71cb2121258bda35d171b
rizkingrh <rizkingrh.mhd@gmail.com>
2024-06-15 15:55:56 +0700

    delete search

--- resources/views/pengadaan/show.blade.php
+++ resources/views/pengadaan/show.blade.php
@@ -116,30 +116,14 @@
                         placeholder="PGN000" required="" value="{{ $data->nomer_pengadaan }}" disabled readonly>
                 </div>
                 <div>
-                    <label for="kode_supplier" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kode
-                        Supplier</label>
-                    <input type="text" name="kode_supplier" id="kode_supplier"
+                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama
+                        Petugas/User</label>
+                    <input type="text" name="name" id="name"
                         class="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                        placeholder="PGN000" required="" value="{{ $data->supplier->kode_supplier }}" disabled
-                        readonly>
+                        placeholder="PGN000" required="" value="{{ $data->user->name }}" disabled readonly>
                 </div>
             </div>
-            <div class="pb-4 bg-white flex justify-between items-center">
-                <form action="{{ url('pengadaan-item') }}" id="searchForm" name="searchForm" method="get">
-                    <label for="search" class="sr-only">Search</label>
-                    <div class="relative mt-1">
-                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                            </svg>
-                        </div>
-                        <input type="text" id="searchKey" name="searchKey"
-                            class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-                            placeholder="Pencarian item" value="{{ Request::get('searchKey') }}">
-                    </div>
-                </form>
+            <div class="pb-6 bg-white flex justify-end items-center">
                 <button type="button" data-modal-target="create-modal" data-modal-toggle="create-modal"
                     class="px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                     <svg class="w-4 h-4 text-white me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
